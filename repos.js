a9db6802f273172fa8af2d2b35f8d2c07cf9e37a
maji <150906506+maji-git@users.noreply.github.com>
2025-04-03 10:06:30 +0700

    Rearrange order

--- repos.js
+++ repos.js
@@ -1,7 +1,7 @@
 export default [
+    "maji-git/mpc-addon-discovery",
     "maji-git/packrtc-mpc",
     "maji-git/multiplay-rivet",
-    "maji-git/mpc-addon-discovery",
     "maji-git/multiplay-demo-platformer-2d",
     "maji-git/mpc-gui-demo"
 ]
